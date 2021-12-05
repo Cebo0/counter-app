24e5383c6c3da7b84228a230f79b4b378fc1b929
Cebo0 <servet.ceberut@gmail.com>
2021-12-05 20:46:57 +0300

    lesson 16

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,14 +4,31 @@ class Counter extends React.Component {
   state = {
     count: 0,
   };
+
+  handleIncrement = (product) => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <buttton>Increment</buttton>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <buttton
+          onClick={() => this.handleIncrement(product)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </buttton>
       </React.Fragment>
     );
   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
