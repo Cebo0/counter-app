c94d61ced5df683dcdbaec803da83eb60447e6dc
Servet Ceberut <servet.ceberut@gmail.com>
2021-12-06 18:10:28 +0300

    lesson 8 composing components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 
 class Counter extends React.Component {
   state = {
-    count: 0,
+    value: this.props.value,
   };
 
-  handleIncrement = (product) => {
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
+  handleIncrement = () => {
+    console.log();
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
@@ -15,22 +15,28 @@ class Counter extends React.Component {
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <buttton
-          onClick={() => this.handleIncrement(product)}
+          onClick={() => this.handleIncrement()}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </buttton>
+        <button
+          onClick={this.props.onDelete}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </React.Fragment>
     );
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
